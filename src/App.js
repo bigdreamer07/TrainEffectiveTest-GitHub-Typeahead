84bc30d1338b7e0481b26a11668b2c12cbebbe6d
bigdreamer07 <lancerstar2020@outlook.com>
2020-12-31 02:23:00 +0800

    fav icon changed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './assets/App.css';
 import Typeahead from './components/Typeahead.js'
 
 function App() {
+  const logo = 'https://media-exp1.licdn.com/dms/image/C4E0BAQFCPsksHVB7Jw/company-logo_100_100/0/1519867276417?e=1617235200&v=beta&t=uDV9q0X5zAJKjsjAbkQHCr-xMX6o-yLz8UjT5ALodr4';
   return (
     <div className="App">
       <div className="App-content">
-        <img src={'https://media-exp1.licdn.com/dms/image/C4E0BAQFCPsksHVB7Jw/company-logo_100_100/0/1519867276417?e=1617235200&v=beta&t=uDV9q0X5zAJKjsjAbkQHCr-xMX6o-yLz8UjT5ALodr4'} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <h2 className="App-title">Github User Typeahead</h2>
         <Typeahead />
       </div>
